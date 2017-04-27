bca6488b5fd7a7cc668768d4b8d69b51530c8410
chrisjdavie <cjdavie@googlemail.com>
2017-04-27 14:34:04 +0000

    eye and identity solved

---
+++ python/numpy/eye_and_identity/run.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+
+N, M = map(int, input().split())
+
+print(np.eye(N,M))
+
---
+++ python/numpy/zeros_and_ones/run.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+
+dims = list(map(int, input().split()))
+
+print(np.zeros(dims,int))
+print(np.ones(dims,int))
+
