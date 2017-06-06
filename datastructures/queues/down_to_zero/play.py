9b2a8270879ca69876fb6424f2061f81a0984608
chrisjdavie <cjdavie@googlemail.com>
2017-06-06 10:56:45 +0000

    fast enough - integer lists are faster than bool lists?

--- datastructures/queues/down_to_zero/play.py
+++ datastructures/queues/down_to_zero/play.py
@@ -1,22 +1,30 @@
 
-from queue import Queue
+from math import sqrt
 
 def play(initial_val):
     
     if initial_val < 1:
         return initial_val
 
-    vals_to_process = Queue()
-    vals_to_process.put((initial_val, 1))
+    vals_to_process = []
+    vals_to_process.append((initial_val, 1))
+    visited = [ 0 ] * initial_val
 
-    while not vals_to_process.empty():
+    while vals_to_process:
 
-        val, moves = vals_to_process.get()
+        val, moves = vals_to_process.pop(0)
         if val == 1:
             return moves
 
-        vals_to_process.put((val-1, moves+1))
-        for i in range(2, int(val**0.5) + 1):
+        i = int(sqrt( val ))
+        while i > 1:
             if not val%i:
-                vals_to_process.put((val//i, moves+1))
-    
+                new_val = val//i
+                if not visited[new_val]:
+                    vals_to_process.append((new_val, moves+1))
+                    visited[new_val] = 1
+            i -= 1
+
+        if not visited[val-1]:
+            vals_to_process.append((val-1, moves+1))
+            visited[val-1] = 1
